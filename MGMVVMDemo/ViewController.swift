cb08c9fc1d65aa77e36d201718c40803cdedaf1b
Truong Anh Tuan <tuan188@gmail.com>
2016-05-24 10:58:11 +0700

    Init project

---
+++ MGMVVMDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MGMVVMDemo
+//
+//  Created by Tuan Truong on 5/24/16.
+//  Copyright © 2016 Tuan Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
